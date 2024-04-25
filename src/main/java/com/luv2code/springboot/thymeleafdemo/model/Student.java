5d77cb37dc4aa8eab26a67b063ea158185fc1c03
Derek Hayes <derek2099@gmail.com>
2024-04-25 13:28:14 -0500

    add radio buttons

--- src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -5,7 +5,7 @@ public class Student {
     private String firstName;
     private String lastName;
     private String country;
-
+    private String favoriteLanguage;
 
 
     public String getFirstName() {
@@ -30,5 +30,13 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
     }
 }
